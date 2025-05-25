e6c5d9051deb2dfbd6905c6c999d025c18689408
Alex <81694529+Clepfeed@users.noreply.github.com>
2025-05-25 23:54:13 +0300

    feature-task3

---
+++ task2-2/task2-2/FillStyle.swift
@@ -0,0 +1,14 @@
+//
+//  FillStyle.swift
+//  task2-2
+//
+//  Created by Ivan Gorokhov on 5/11/25.
+//
+
+import Foundation
+
+enum FillStyle: Int {
+    case solid
+    case gradient
+    case shadow
+}
---
+++ task2-2/task2-2/ShapeType.swift
@@ -0,0 +1,16 @@
+//
+//  ShapeType.swift
+//  task2-2
+//
+//  Created by Ivan Gorokhov on 5/11/25.
+//
+
+import Foundation
+
+enum ShapeType: Int {
+    case triangle
+    case hexagon
+    case union
+    case subtract
+}
+
